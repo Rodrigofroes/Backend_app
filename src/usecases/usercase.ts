0e14c6027dbfbf414fa8e10ce228415cdcb81144
RodrigoFroes <craftland262@gmail.com>
2024-09-02 21:46:01 -0300

    Feat: Alteração no nome do arquivo

--- src/usecases/usercase.ts
+++
@@ -1,4 +0,0 @@
-// contrato de uso de caso de uso, por de entrada de controller para caso de uso
-export interface Usecase<InputDto, OutputDto> {
-    execute(input: InputDto): Promise<OutputDto>;
-}
